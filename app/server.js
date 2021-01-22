db1708b84a7604689feb87c7f6008388e06f3921
anderson-ribeiro-mobcall <anderson.ribeiro@mobcall.com>
2021-01-22 09:28:37 -0300

    ajustes de url local

--- app/server.js
+++ app/server.js
@@ -2,5 +2,5 @@ const app = require('./app')
 
 const port = process.env.port || 4001
 module.exports = app.listen(port, () => {
-    console.log('server na porta', port)
+    console.log(`server http://localhost:${port}/`)
 })
