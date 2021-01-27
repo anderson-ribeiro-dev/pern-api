113eccfdba0a404abf500fe0671e8ad6029fc56e
anderson-ribeiro-mobcall <anderson.ribeiro@mobcall.com>
2021-01-27 18:36:55 -0300

    process.env.port

--- app/server.js
+++ app/server.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 
-const port = process.env.PORT || 4001;
+const port = process.env.PORT || 8081;
 module.exports = app.listen(port, () => {
   console.log(`server http://localhost:${port}/`);
 });
