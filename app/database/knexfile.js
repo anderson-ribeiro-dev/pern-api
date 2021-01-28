77babe69d27eec24ee7aa0e2a69161e588fbbdd4
anderson-ribeiro-mobcall <anderson.ribeiro@mobcall.com>
2021-01-28 14:34:50 -0300

    knex file

--- app/database/knexfile.js
+++ app/database/knexfile.js
@@ -2,19 +2,21 @@ require("dotenv").config();
 
 module.exports = {
   client: "pg",
+  ssl: true,
   connection: {
     host: "produtos.cd33u3w7gwiu.us-east-1.rds.amazonaws.com",
     port: "5432",
     user: "postgres",
     password: "derso_1804****",
     database: "postgres",
+    ssl: true,
   },
   // connection: {
-  //   host : 'localhost',
-  //   port : 5432,
-  //   user: 'postgres',
-  //   password: '123456',
-  //   database: 'autenticacao'
+  //   host: "localhost",
+  //   port: "5432",
+  //   user: "postgres",
+  //   password: "123456",
+  //   database: "autenticacao",
   // },
   pool: {
     min: 0,
