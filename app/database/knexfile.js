35fcd89ec44e0f019bbb0a621f5e0336e9b3edd1
anderson-ribeiro-mobcall <anderson.ribeiro@mobcall.com>
2021-01-28 17:41:34 -0300

    knex file

--- app/database/knexfile.js
+++ app/database/knexfile.js
@@ -1,6 +1,34 @@
-module.exports = {
+// module.exports = {
+//   client: "pg",
+//   ssl: true,
+//   connection: {
+//     host: "produtos.cd33u3w7gwiu.us-east-1.rds.amazonaws.com",
+//     port: 5432,
+//     user: "postgres",
+//     password: "derso_1804****",
+//     database: "postgres",
+//     ssl: true,
+//   },
+//   // connection: {
+//   //   host: "localhost",
+//   //   port: "5432",
+//   //   user: "postgres",
+//   //   password: "123456",
+//   //   database: "autenticacao",
+//   // },
+//   pool: {
+//     min: 2,
+//     max: 10,
+//   },
+//   migrations: {
+//     tableName: "knex_migrations",
+//   },
+// };
+
+const cnn = {
   client: "pg",
   ssl: true,
+  sslmode: "require",
   connection: {
     host: "produtos.cd33u3w7gwiu.us-east-1.rds.amazonaws.com",
     port: 5432,
@@ -9,18 +37,14 @@ module.exports = {
     database: "postgres",
     ssl: true,
   },
-  // connection: {
-  //   host: "localhost",
-  //   port: "5432",
-  //   user: "postgres",
-  //   password: "123456",
-  //   database: "autenticacao",
-  // },
-  pool: {
-    min: 2,
-    max: 10,
-  },
+  debug: false,
+  pool: { min: 0, max: 15 },
   migrations: {
     tableName: "knex_migrations",
   },
+};
+module.exports = {
+  development: cnn,
+  staging: cnn,
+  production: cnn,
 };
