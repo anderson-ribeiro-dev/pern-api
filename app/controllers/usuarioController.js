011e24fdd0f6c8fe5ebfcd9af9871b40c3f05b8a
anderson-ribeiro-mobcall <anderson_silvaribeiro@hotmail.com>
2021-01-29 17:36:43 -0300

    knec file

--- app/controllers/usuarioController.js
+++ app/controllers/usuarioController.js
@@ -1,27 +1,30 @@
-const usuarioModel = require('../models/usuarioModel')
-const bcrypt = require('bcrypt')
+const usuarioModel = require("../models/usuarioModel");
+const bcrypt = require("bcrypt");
 
 class UsuarioController {
-    async index(req, res){}
+  async index(req, res) {
+    const email = "andersonsilvaribeiro@gmail.com";
+    const usuario = await usuarioModel.usuario(email);
+    res.json(usuario);
+  }
 
-    async create(req, res){ 
-        const data = req.body
-        data['password'] = await bcrypt.hash(data.password, 8)
-        data['role'] = 1
+  async create(req, res) {
+    const data = req.body;
+    data["password"] = await bcrypt.hash(data.password, 8);
+    data["role"] = 1;
 
-        const result = await usuarioModel.cadastrarUsuario(data)
-        if(result[0] !== undefined){
-            res.status(200)
-            res.json(result)
-        }else{
-            res.status(400)
-            res.json(result.detail)
-        }      
-    }  
+    const result = await usuarioModel.cadastrarUsuario(data);
+    if (result[0] !== undefined) {
+      res.status(200);
+      res.json(result);
+    } else {
+      res.status(400);
+      res.json(result.detail);
+    }
+  }
 
-    async edit (req, res){}
-    async delete (req, res){}
-
+  async edit(req, res) {}
+  async delete(req, res) {}
 }
 
-module.exports = new UsuarioController()
+module.exports = new UsuarioController();
