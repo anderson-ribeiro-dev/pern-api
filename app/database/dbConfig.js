5cd15792b2853f31260d9a75b3fd73d0f33726db
anderson-ribeiro-mobcall <anderson_silvaribeiro@hotmail.com>
2021-01-29 17:47:56 -0300

    knec file

--- app/database/dbConfig.js
+++ app/database/dbConfig.js
@@ -1,2 +1,5 @@
-const config = require("./knexfile");
+require("dotenv").config();
+const env = process.env.environment || "development";
+
+const config = require("./knexfile")[env];
 module.exports = require("knex")(config);
--- app/database/knexfile.js
+++ app/database/knexfile.js
@@ -1,26 +1,29 @@
 module.exports = {
-  client: "pg",
-  connection: {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-  },
-  // connection: {
-  //   host: "localhost",
-  //   port: "5432",
-  //   user: "postgres",
-  //   password: "123456",
-  //   database: "autenticacao",
-  // },
-  debub: true,
-  pool: {
-    min: 0,
-    max: 15,
-  },
-  migrations: {
-    directory: "migrations",
-    tableName: "knex_migrations",
+  development: {
+    client: "pg",
+    connection: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      connectTimeout: 90000,
+    },
+    // connection: {
+    //   host: "localhost",
+    //   port: "5432",
+    //   user: "postgres",
+    //   password: "123456",
+    //   database: "autenticacao",
+    // },
+    debub: true,
+    pool: {
+      min: 1,
+      max: 20,
+    },
+    migrations: {
+      directory: "migrations",
+      tableName: "knex_migrations",
+    },
   },
 };
