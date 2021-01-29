da9b8bff3c28fe0900e46f79399e7526a3ca38f5
anderson-ribeiro-mobcall <anderson_silvaribeiro@hotmail.com>
2021-01-29 16:14:39 -0300

    knec file

--- app/database/knexfile.js
+++ app/database/knexfile.js
@@ -2,11 +2,11 @@ module.exports = {
   default: {
     client: "pg",
     connection: {
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
+      host: "produtos.cd33u3w7gwiu.us-east-1.rds.amazonaws.com",
+      port: "5432",
+      user: "postgres",
+      password: "derso_1804****",
+      database: "postgres",
     },
     // connection: {
     //   host: "localhost",
